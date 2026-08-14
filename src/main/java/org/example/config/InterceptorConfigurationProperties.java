c08fb7b29531ee3d88cd49d3dd0f57844b2364a7
IvanHoncharenko43 <ivangoncharenko200714@gmail.com>
2026-08-14 13:04:00 +0300

    Cleaned up old config files and pom.xml

--- src/main/java/org/example/config/InterceptorConfigurationProperties.java
+++
@@ -1,9 +0,0 @@
-package org.example.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "interceptor")
-public record InterceptorConfigurationProperties(
-        String includePath
-) {
-}
