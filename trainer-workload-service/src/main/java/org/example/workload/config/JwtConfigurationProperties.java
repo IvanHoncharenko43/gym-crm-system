055e0b12d94b62faaf4aac333d8ec9d44b57daa6
IvanHoncharenko43 <ivangoncharenko200714@gmail.com>
2026-08-21 09:50:48 +0300

    Added Spring Security layer to the trainer-workload-service

---
+++ trainer-workload-service/src/main/java/org/example/workload/config/JwtConfigurationProperties.java
@@ -0,0 +1,10 @@
+package org.example.workload.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtConfigurationProperties(
+        String secretKey,
+        Long expiration
+) {
+}
