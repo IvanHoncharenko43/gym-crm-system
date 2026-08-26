2470d3f1ec15b2dc7d97f02d9bb2da657f0fdc03
IvanHoncharenko43 <ivangoncharenko200714@gmail.com>
2026-08-26 11:21:03 +0300

    Refactored ClientConfig to use DiscoveryClient instead of configuring builder beans manually

--- crm-service/src/main/java/org/example/crm/config/ClientConfig.java
+++ crm-service/src/main/java/org/example/crm/config/ClientConfig.java
@@ -2,18 +2,19 @@ package org.example.crm.config;
 
 import lombok.RequiredArgsConstructor;
 import org.example.crm.trainer.controller.TrainerWorkloadClient;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.time.Duration;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,35 +22,31 @@ import java.time.Duration;
 public class ClientConfig {
 
     private final ClientConfigurationProperties clientConfigurationProperties;
-
-    @Bean
-    @Primary
-    public RestClient.Builder defaultRestClientBuilder() {
-        return RestClient.builder();
-    }
-
-    @Bean
-    @LoadBalanced
-    public RestClient.Builder loadBalancedRestClientBuilder() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(Duration.ofSeconds(clientConfigurationProperties.connectTimeout()));
-        factory.setReadTimeout(Duration.ofSeconds(clientConfigurationProperties.readTimeout()));
-        return RestClient.builder()
-                .requestFactory(factory);
-    }
+    private final DiscoveryClient discoveryClient;
 
     @Bean
     public TrainerWorkloadClient trainerWorkloadClient(
-            @Qualifier("loadBalancedRestClientBuilder") RestClient.Builder restClientBuilder,
+            RestClient.Builder restClientBuilder,
             TokenPopulationInterceptor tokenPopulationInterceptor,
             TraceIdPopulationInterceptor traceIdPopulationInterceptor){
+
+        List<ServiceInstance> instances = discoveryClient.getInstances(clientConfigurationProperties.workloadId());
+        String resolveUrl = instances.getFirst().getUri().toString();
         RestClient restClient = restClientBuilder
-                .baseUrl(clientConfigurationProperties.workloadUrl())
+                .baseUrl(resolveUrl)
+                .requestFactory(clientHttpRequestFactory())
                 .requestInterceptor(tokenPopulationInterceptor)
                 .requestInterceptor(traceIdPopulationInterceptor)
                 .build();
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
         return factory.createClient(TrainerWorkloadClient.class);
+    }
+
+    private ClientHttpRequestFactory clientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(clientConfigurationProperties.connectTimeout()));
+        factory.setReadTimeout(Duration.ofSeconds(clientConfigurationProperties.readTimeout()));
+        return factory;
     }
 }
--- crm-service/src/main/java/org/example/crm/config/ClientConfigurationProperties.java
+++ crm-service/src/main/java/org/example/crm/config/ClientConfigurationProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "client.services")
 public record ClientConfigurationProperties(
-        String workloadUrl,
+        String workloadId,
         Long connectTimeout,
         Long readTimeout
 ) {
