e8f3773cc44d6616d452d6e5b3df33d907bb0164
IvanHoncharenko43 <ivangoncharenko200714@gmail.com>
2026-09-04 00:20:05 +0300

    Added traceId to successful and failed handlings after message publishing

--- crm-service/src/main/java/org/example/crm/trainer/messaging/TrainerWorkloadProducerService.java
+++ crm-service/src/main/java/org/example/crm/trainer/messaging/TrainerWorkloadProducerService.java
@@ -5,11 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.example.crm.config.KafkaTopicsConfigurationProperties;
 import org.example.crm.config.RequestHeaderContextResolver;
+import org.slf4j.MDC;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import static org.example.crm.core.filter.TraceIdFilter.TRACE_ID_KEY;
 
@@ -31,12 +33,20 @@ public class TrainerWorkloadProducerService {
 
         requestHeaderContextResolver.getTraceIdHeader()
                 .ifPresent(traceIdHeader -> record.headers().add(TRACE_ID_KEY, traceIdHeader.getBytes(StandardCharsets.UTF_8)));
+        Map<String, String> mdcContext = MDC.getCopyOfContextMap();
         kafkaTemplate.send(record)
                 .whenComplete((result, ex) -> {
-                    if (ex == null) {
-                        log.info("Successfully sent workload update for trainer");
-                    } else {
-                        log.error("Failed to send workload update for trainer", ex);
+                    if(mdcContext != null){
+                        MDC.setContextMap(mdcContext);
+                    }
+                    try {
+                        if (ex == null) {
+                            log.info("Successfully sent workload update for trainer");
+                        } else {
+                            log.error("Failed to send workload update for trainer", ex);
+                        }
+                    } finally {
+                        MDC.clear();
                     }
                 });
     }
